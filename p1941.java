0fc343fe0feb3e0dc55d77fcefcff278a0ccc216
aayushpawar07 <ayushparadkar4@gmail.com>
2025-04-17 20:47:51 +0530

    Add initial file

--- p1941.java
+++ p1941.java
@@ -1,4 +1,4 @@
-
+import java.util.HashMap;
 
 public class p1941{
 
@@ -16,23 +16,44 @@ public class p1941{
 
     public static  boolean areOccurrencesEqual(String s){
       
-        int [] count = new int[26];
+        // int [] count = new int[26];
 
-        for(int i : s.toCharArray()){
-            count[i - 'a']++;
-        }
+        // for(int i : s.toCharArray()){
+        //     count[i - 'a']++;
+        // }
         
-        int freq = 0;
+        // int freq = 0;
          
-        for( int i : count){
+        // for( int i : count){
 
-            if( freq == 0 && i != 0){
-                freq = i;
-            }
-            else if( i != 0 && i != freq){
+        //     if( freq == 0 && i != 0){
+        //         freq = i;
+        //     }
+        //     else if( i != 0 && i != freq){
+        //         return false;
+        //     }
+        // }
+
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            map.put(c , map.getOrDefault(c , 0 )+1);
+        }
+     
+        int freq = map.get(s.charAt(0));
+
+
+        for( int i : map.values()){
+          
+            if( i != freq){
                 return false;
             }
+
         }
+
+
+
 
 
  return true;
@@ -42,7 +63,7 @@ public class p1941{
 
 
 
-    
+
   
 
 
